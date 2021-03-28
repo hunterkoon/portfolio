415192382eeb9de15e8be3af3e8f976ba515a75b
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-03-28 19:01:41 -0300

    append javascript

--- myScript.js
+++ myScript.js
@@ -1,20 +1,45 @@
 
+        
+    
+            
+            let image = document.getElementsByName('imagem');  
+            const classRotate = document.getElementsByClassName('section_rotate');
+
+            for (let i = 0 ; i < image.length ; i++){        
+
+                classRotate[i].addEventListener('click',
+                function(e){
+                    recolher ()
+                    if(image[i].style.transform != 'rotate(90deg)'){
+                        image[i].style.transform = 'rotate(90deg)';
+            
+            } else {
+                        image[i].style.transform = 'rotate(0deg)';
+
+                }
+                }
+                )
+
+            }
+
+  
 
 
-
-    function recolher () {
-var listseq = document.querySelectorAll('section');
-const sequeCheck = [listseq[5],listseq[7],listseq[9],listseq[11]];
-
-for (var i = 0; i < 4; i++ ) {    
-    if(sequeCheck[i].style.display === 'block'){
+function recolher () {
+    
+    var listseq = document.querySelectorAll('section');
+    
+    const sequeCheck = [listseq[5],listseq[7],listseq[9],listseq[11]];
+    
+    for (let i = 0; i < 4; i++ ) {    
+        
+    if(sequeCheck[i].style.display === 'block'){        
         sequeCheck[i].style.display = 'none'; 
     }   
     console.log(sequeCheck[i]); 
     }
+
 } 
-
-
 
 
 
@@ -25,9 +50,22 @@ for (var i = 0; i < 4; i++ ) {
 
     section[1].style.display ='block';
 
-    }else 
-    section[1].style.display = 'none';
-   
+    } else  { 
+    section[1].style.display = 'none'; } 
+    
+    section[0].animate([
+    // keyframes
+    { transform: 'translateY(0px)' },
+    { transform: 'translateY(-10px)' }
+  ], {
+    // timing options
+    duration: 300,
+    iterations: 1,
+  });
+
 
 
 }
+
+console.log("hello world!")
+
