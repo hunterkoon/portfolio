0f9b56dc25c17a7a9f8da0df68e24de49553794a
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-03-30 17:46:34 -0300

    js replaced

--- myScript.js
+++ myScript.js
@@ -1,89 +1,96 @@
-           
-let image = document.getElementsByName('imagem');  
+// INICIALIZA VARIAVEIS 
+
+
+// CAPTURA ELEMENTOS DA DIV - EMPRESAS LADO ESQUERDO - TODA A DIV
+var sectionDivs = document.getElementsByName('divClick');
+
+
+// CAPTURA ELEMENTOS DA DIV  - EMPRESAS LADO ESQUERDO - TEXTOS
+var sectionEmpresasText = document.getElementsByName('section_text');
+
+// CAPTURA ELEMENTOS SPAN - EMPRESAS LADO ESQUERDO - SPAN
 const classRotate = document.getElementsByClassName('section_rotate');
-  
-    for (let i = 0 ; i < image.length ; i++){        
-       classRotate[i].addEventListener('click',function(){
-           if(image[i].style.transform != 'rotate(90deg)'){               
+
+// CAPTURA - EMPRESAS LADO ESQUERDO - ICONES
+let image = document.getElementsByName('imagem');
+
+// ---------------------------------------||--------------------------------------
+
+
+// CAPTURA ELEMENTOS DA TEXTBOX - DESCRIÇÃO DE AFAZERES -- PARAGRAFO
+var textBoxp = document.querySelectorAll('.text_box p');
+
+// CAPTURA ELEMENTOS  - EMPRESAS LADO DIREITO - SECTION
+var sectionPtext = document.getElementsByName('textBoxHide');
+
+
+
+// ---------------------------------------||--------------------------------------
+
+
+// ANIMAÇÃO ICONES
+
+for (let i = 0; i < image.length; i++) {
+    classRotate[i].addEventListener('click', function() {
+            if (image[i].style.transform != 'rotate(90deg)') {
                 image[i].style.transition = '0.5s';
                 image[i].style.transform = 'rotate(90deg)';
-           } else {
-               image[i].style.transform = 'rotate(0deg)';
+            } else {
+                image[i].style.transform = 'rotate(0deg)';
             }
-         })// FUNÇÃO FIM
+        }) // FUNÇÃO FIM
 }
 
 // RECOLHE TEXTOS EXIBIDOS
 
-function recolher () {
-    
-    var listseq = document.querySelectorAll('section');    
-    const sequeCheck = [listseq[5],listseq[7],listseq[9],listseq[11]];
-    
-    for (let i = 0; i < 4; i++ ) {    
-        
-    if ( sequeCheck[i].style.display === 'block'){        
-        sequeCheck[i].style.display = 'none'; 
-    } 
-    console.log(sequeCheck[i]); 
+function recolher() {
+
+    var listseq = document.querySelectorAll('section');
+    const sequeCheck = [listseq[5], listseq[7], listseq[9], listseq[11]];
+
+    for (let i = 0; i < 4; i++) {
+
+        if (sequeCheck[i].style.display === 'block') {
+            sequeCheck[i].style.display = 'none';
+        }
+        console.log(sequeCheck[i]);
     }
-} 
-
-
+}
 
 // MOSTRA TEXTOS SELECIONADOS
 
 function showText(event) {
-    //recolher ()
-    const section = event.querySelectorAll('section');     
-    if(section[1].style.display === 'none') {
-        
-        section[1].style.display ='block';
-    } else  { 
-        section[1].style.display = 'none'; } ;
-        
-        //Animação 
-        
-        section[0].animate([
-            // keyframes
-    { transform: 'translateY(0px)' },
-    { transform: 'translateY(-10px)' }
-], {
-    // timing options
-    duration: 200,
-    iterations: 1,
 
-});
+    const section = event.querySelectorAll('section');
+    if (section[1].style.display === 'none') {
+        section[1].style.display = 'block';
+
+    } else {
+        section[1].style.display = 'none';
+    };
+
+    //Animação 
+
+    section[0].animate([
+        // keyframes
+        { transform: 'translateY(0px)' },
+        { transform: 'translateY(-10px)' }
+    ], {
+        // timing options
+        duration: 200,
+        iterations: 1,
+
+    });
 
 }
 
-var section_1 = document.querySelectorAll('.empresas_list_section div');
-var sectionPtext = document.querySelectorAll('.text_box section');
+function rec(a) {
 
-// 0-cheil, 1-dematec, 2-wv design, 3-digital
+    for (let i = 0; i <= a.length; i++) {
 
-
-
-
-for (let i=0 ; i <= sectionPtext.length ; i++ ){
-    
-    section_1[i].addEventListener('click', function() {   
-        if(sectionPtext[i].style.display === 'none'){ 
-        sectionPtext[i].style.display = 'block'}else {
-            sectionPtext[i].style.display = 'none'
+        if (a[i].style.display === 'block') {
+            a[i].style.display = 'none';
         }
-        console.log(sectionPtext[i]);  }
-        )}
-
-        
-        function rec (a,b) {
-        
-            for (let i = 0; i <= a.length ; i++ ){    
-                
-            if( a[i].style.display === 'block'){        
-                a[i].style.display = 'none'; 
-            }   
-            console.log(a[b]); 
-            }
-        } 
-        
+        console.log(a);
+    }
+}
